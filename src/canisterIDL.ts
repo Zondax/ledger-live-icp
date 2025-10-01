4db0796d2702e82a3dbb8952cf42e6da62a5396d
Prateek Rathod <prateek@zondax.ch>
2025-10-01 23:01:09 +0200

    chore: minor bug fix missing template

--- src/canisterIDL.ts
+++ src/canisterIDL.ts
@@ -6,18 +6,18 @@ import invariant from 'invariant'
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { ledgerIdlFactory, indexIdlFactory, governanceIdlFactory, governanceIdlFactoryOld } = require('../idl-cjs/index.cjs')
 
-export { ledgerIdlFactory, indexIdlFactory, governanceIdlFactory, governanceIdlFactoryOld }
-
-export const getCanisterIdlFunc = (idlFactory: any, methodName: string) => {
-  const func = idlFactory({ IDL })._fields.find((f: any[]) => f[0] === methodName)
+export const getCanisterIdlFunc = (idlFactory: IDL.InterfaceFactory, methodName: string) => {
+  const func = idlFactory({ IDL })._fields.find((f: any) => f[0] === methodName)
   invariant(func, `[ICP](getCanisterIdlFunc) Method ${methodName} not found`)
   return func[1]
 }
 
-export const encodeCanisterIdlFunc = (func: any, args: any[]) => {
+export const encodeCanisterIdlFunc = (func: IDL.FuncClass, args: any) => {
   return IDL.encode(func.argTypes, args)
 }
 
-export const decodeCanisterIdlFunc = (func: any, args: ArrayBuffer) => {
-  return IDL.decode(func.retTypes, args)
+export const decodeCanisterIdlFunc = <T>(func: IDL.FuncClass, args: ArrayBuffer): T => {
+  return IDL.decode(func.retTypes, args) as T
 }
+
+export { ledgerIdlFactory, indexIdlFactory, governanceIdlFactory, governanceIdlFactoryOld }
